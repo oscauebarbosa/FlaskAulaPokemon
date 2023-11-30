4e77770c8b949be857f5dc23c472c68e6a5a7120
oscauebarbosa <124931002+oscauebarbosa@users.noreply.github.com>
2023-11-30 10:26:16 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -33,6 +33,32 @@ def criar():
     lista.append(obj)
     return redirect('/pokemon')
 
+@app.route('/excluir/<numeropkm>', methods=['GET','DELETE'])
+def excluir(numeropkm) :
+    for i, pkm in enumerate(lista):
+        if pkm.numero == numeropkm:
+            lista.pop(i)
+            break
+    return redirect('/pokemon')
+
+@app.route('/editar/<numeropkm>', methods=['GET'])
+def editar(numeropkm):
+    for i, pkm in enumerate(lista):
+        if pkm.numero == numeropkm:
+            return render_template("Editar.html", pokemon=pkm, Titulo="Alterar Pokemon")
+
+@app.route('/alterar', methods = ['POST','PUT'])
+def alterar():
+    numero = request.form['numero']
+    for i, pkm in enumerate(lista):
+        if pkm.numero == numero:
+            pkm.nome = request.form['nome']
+            pkm.tipo = request.form['tipo']
+            pkm.altura = request.form['altura']
+            pkm.peso = request.form['peso']
+        return redirect('/pokemon')
+
+
 
 
 if __name__ == '__main__':
